3da13d808d5d8f228fe2f385fee4e4034b57df95
Whirlpion <whirlpion@protonmail.com>
2022-12-30 19:11:37 +0000

    digit_tool: added a ton of functionality
    - multiple cell highlight modes
    - simultaneous cell entry
    - cell content deletion

--- source/scripts/tools/digit_tool.ts
+++ source/scripts/tools/digit_tool.ts
@@ -1,28 +1,35 @@
-class WriteDigitAction extends IAction{
+class WriteDigitAction extends IAction {
     override apply(): void {
-        this.puzzleGrid.setDigitAtCell(this.cell, this.digit);
+        for (let cell of this.cells) {
+            this.puzzleGrid.setDigitAtCell(cell, this.digit);
+        }
     }
     override revert(): void {
-        this.puzzleGrid.setDigitAtCell(this.cell, this.previousDigit);
+        for(let k = 0; k < this.cells.length; k++) {
+            this.puzzleGrid.setDigitAtCell(this.cells[k], this.previousDigits[k]);
+        }
     }
 
     puzzleGrid: PuzzleGrid;
-    cell: Cell;
-    digit: Digit;
-    previousDigit: Digit | null = null;
+    digit: Digit | null;
+    cells: Array<Cell>;
+    previousDigits: Array<Digit | null> = new Array();
 
-    constructor(puzzleGrid: PuzzleGrid, cell: Cell, digit: Digit, previousDigit: Digit | null) {
-        super(`write ${digit} digit to ${cell}`);
+    constructor(puzzleGrid: PuzzleGrid, digit: Digit | null, ...cells: Cell[]) {
+        super(`write ${digit} digit to ${cells}`);
         this.puzzleGrid = puzzleGrid;
-        this.cell = cell;
+        this.cells = cells;
         this.digit = digit;
-        this.previousDigit = previousDigit;
+
+        for(let cell of cells) {
+            this.previousDigits.push(puzzleGrid.getDigitAtCell(cell));
+        }
     }
 }
 
 class DigitTool extends ITool {
-    // the cells which are currently highlighted
-    highlights: BSTSet<Cell> = new BSTSet();
+    // the cells which are currently highlighted mapped to their associated SVG Rect
+    highlightedCells: BSTMap<Cell, SVGRectElement> = new BSTMap();
     // a group containing a list of elements used to highlight a cell
     highlightSvg: SVGGElement;
     // the cell that has focus
@@ -35,11 +42,7 @@ class DigitTool extends ITool {
         this.puzzleGrid = puzzleGrid;
     }
 
-    override handleMouseClick(event: MouseEvent) {
-        let cell = Cell.fromXY(event.offsetX, event.offsetY);
-        this.focusedCell = cell;
-
-        this.highlightSvg.clearChildren();
+    private createHighlightRect(cell: Cell): SVGRectElement {
         let rect = this.sceneManager.createElement("rect", SVGRectElement);
         rect.setAttributes(
             ["width", `${CELL_SIZE}`],
@@ -47,29 +50,118 @@ class DigitTool extends ITool {
             ["fill", Colour.LightBlue.toString()],
             ["x", `${cell.j * CELL_SIZE}`],
             ["y", `${cell.i * CELL_SIZE}`]);
-
-        this.highlightSvg.appendChild(rect);
+        return rect;
     }
 
-    override handleMouseMove(_event: MouseEvent) {
-        // console.log(`move: ${event.offsetX},${event.offsetY}`);
+    // ctrl+click toggles individual cells
+    // shift+click should select all cells between current and previous click
+    // click clears current selection set and sets current cell
+    override handleMouseDown(event: MouseEvent) {
+        // only care about 'left' click
+        if (!event.primaryButton) {
+            return;
+        }
+
+        // unclear which thing the user wants to do, so best to do nothing
+        if (event.shiftKey && event.shortcutKey) {
+            return;
+        }
+
+        const cell = Cell.fromMouseEvent(event);
+
+        if (event.shortcutKey) {
+            // cell toggle
+            let rect = this.highlightedCells.get(cell);
+            if (rect) {
+                this.highlightedCells.delete(cell);
+                this.highlightSvg.removeChild(rect);
+                // no focused cell after this point
+                this.focusedCell = null;
+            } else {
+                rect = this.createHighlightRect(cell);
+                this.highlightedCells.set(cell, rect);
+                this.highlightSvg.appendChild(rect);
+                // focus the cell
+                this.focusedCell = cell;
+            }
+        } else if (event.shiftKey && this.focusedCell) {
+            // cell line
+            this.highlightLine(this.focusedCell, cell);
+        } else {
+            // set only the cell
+            this.highlightedCells.clear();
+            this.highlightSvg.clearChildren();
+
+            const rect = this.createHighlightRect(cell);
+            this.highlightedCells.set(cell, rect);
+            this.highlightSvg.appendChild(rect);
+            // focus the cell
+            this.focusedCell = cell;
+        }
+    }
+
+    override handleMouseUp(_event: MouseEvent) {
+        // console.log(`mouseup: ${event.offsetX},${event.offsetY}`);
+    }
+
+    override handleMouseMove(event: MouseEvent) {
+        // only care about click and drag
+        if (!event.primaryButton || !this.focusedCell) {
+            return;
+        }
+
+        const cell = Cell.fromMouseEvent(event);
+        this.highlightLine(this.focusedCell, cell);
+    }
+
+    private highlightLine(from: Cell, to: Cell): void {
+        // cell line
+        let line = Cell.bresenhamLine(from, to);
+        for (let cell of line) {
+            let rect = this.highlightedCells.get(cell);
+            if (!rect) {
+                rect = this.createHighlightRect(cell)
+                this.highlightedCells.set(cell, rect);
+                this.highlightSvg.appendChild(rect);
+            }
+        }
+        this.focusedCell = to;
+    }
+
+    private writeDigit(digit: Digit | null): void {
+        // first make sure the key press would result in anything changing
+        let actionIsNoop = true;
+        let cells = Array.collect(this.highlightedCells.keys());
+        for(let cell of cells) {
+            if (this.puzzleGrid.getDigitAtCell(cell) !== digit) {
+                actionIsNoop = false;
+                break;
+            }
+        }
+
+        if(actionIsNoop) {
+            return;
+        }
+
+        let action = new WriteDigitAction(this.puzzleGrid, digit, ...cells);
+        this.actionStack.doAction(action);
     }
 
     override handleKeyDown(event: KeyboardEvent) {
-        if (this.focusedCell) {
+        if (this.highlightedCells.size > 0) {
             switch(event.key) {
             case "1": case "2": case "3":
             case "4": case "5": case "6":
             case "7": case "8": case "9":
-                {
-                    let digit = Digit.parse(event.key);
-                    let previousDigit = this.puzzleGrid.getDigitAtCell(this.focusedCell);
-                    if (digit != previousDigit) {
-                        let action = new WriteDigitAction(this.puzzleGrid, this.focusedCell, digit, previousDigit);
-                        this.actionStack.doAction(action);
-                    }
-                }
-                break
+                this.writeDigit(Digit.parse(event.key));
+                return;
+            default:
+                break;
+            }
+            switch(event.code) {
+            case "Delete": case "Backspace":
+                this.writeDigit(null);
+                return;
             default:
                 break;
             }
